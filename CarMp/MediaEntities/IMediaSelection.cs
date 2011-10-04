fdf8b730119e14d0e94b1f9f9b24b8985a9b6aa5
efessel@gmail.com <efessel@gmail.com@d4bd84e2-f980-fb58-ec8a-1c4e3f86fd2c>
2011-10-04 22:08:39 +0000

    neglected IMediaSelection.cs in previous checkin

---
+++ CarMp/MediaEntities/IMediaSelection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CarMP.MediaEntities;
+
+namespace CarMP
+{
+    public interface IMediaSelection
+    {
+        string DisplayString { get; }
+        MediaListItemType MediaType { get; }
+        string Key { get; }
+        int ItemSpecificType { get; }
+        string ObjectType { get; }
+    }
+}
